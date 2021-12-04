be6aed5dfa4353beeb42967a94b9c671646b6139
RulerOfCakes <fragmentbombs@gmail.com>
2021-12-05 02:29:15 +0900

    tailwind jit mode enabled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
+//const colors=require('tailwindcss/colors')
+
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}','./components/**/*.{js.ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        discord_primary: "#202225",
+        discord_secondary: "#5865f2",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
