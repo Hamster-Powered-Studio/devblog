dafd79ed75c9e799f1ca6cdbaa66f5b88d0d0610
RulerOfCakes <fragmentbombs@gmail.com>
2022-01-19 04:09:18 +0900

    about us description

--- pages/about/index.js
+++ pages/about/index.js
@@ -6,13 +6,22 @@ export default function Home({ allPostsData }) {
     <>
       <Layout title="Hamster Powered DevBlog">
         <div>
-          <h2 className="text-center text-4xl font-sans font-bold">
-            About Us
-          </h2>
+          <h2 className="text-center text-4xl font-sans font-bold">About Us</h2>
         </div>
         <section>
           <h2 className="text-3xl font-semibold mb-7">Introduction</h2>
-          
+          <p>
+            We&apos;re a hobby-oriented game development group, aiming to actively
+            participate in Game Jams such as Global Game Jam and Ludum Dare!
+          </p>
+          <p className="my-4">
+            Currently our group consists of three main members:
+          </p>
+          <p>
+            - Unbanshee: Capable of everything, leader of the group & main designer<br/>
+            - SilverBranch: Blueprint Programmer, UI Designer<br/>
+            - RulerOfCakes: C++ & Bluperint Programmer<br/>
+          </p>
         </section>
       </Layout>
     </>
